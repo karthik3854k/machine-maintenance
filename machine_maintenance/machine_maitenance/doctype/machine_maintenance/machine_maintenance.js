d96ab4f057aa76a49dc0f3e1a6b88e5fddaeae1b
Harish <harish@Harishs-MacBook-Air.local>
2025-11-19 20:27:26 +0530

    What's new:added doctypes

---
+++ machine_maintenance/machine_maitenance/doctype/machine_maintenance/machine_maintenance.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, karthik polisetty and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Machine Maintenance", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ machine_maintenance/machine_maitenance/doctype/machine_maintenance_part/test_machine_maintenance_part.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, karthik polisetty and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMachineMaintenancePart(FrappeTestCase):
+	pass
